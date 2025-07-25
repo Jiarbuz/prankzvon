6045ee6c1b227efc03602703b2856001c17294f3
Jiarbuz <savelijsizyh6@gmail.com>
2025-07-25 10:36:06 +0700

    Грызть какахи

--- app.py
+++ app.py
@@ -6,40 +6,50 @@ import os
 load_dotenv()
 
 app = Flask(__name__)
-app.secret_key = os.getenv('20223b185377e4778f8366afc34f9af0e0f0a88bd7217b52d7f1fbe4460fb554', 'default-secret-key')
+app.secret_key = os.getenv('SECRET_KEY', 'default-secret-key')
 
 app.config['BABEL_DEFAULT_LOCALE'] = 'ru'
 babel = Babel(app)
 
 def get_locale():
-    return session.get('lang') or request.accept_languages.best_match(['ru', 'en'])
+    return session.get('lang', 'ru')
 
 babel.locale_selector_func = get_locale
 
-
-# Словари переводов
+# Complete translations
 translations = {
     'ru': {
         'info_title': "PrankVzlom 📹📔",
-        'disclaimer': "САЙТ СДЕЛАН ДЛЯ РАЗВЛЕКАТЕЛЬНЫХ ЦЕЛЯХ И МЫ НИКОГО НЕ ХОТИМ ОСКОРБИТЬ ИЛИ УНИЗИТЬ",
+        'disclaimer': "САЙТ СДЕЛАН ДЛЯ РАЗВЛЕКАТЕЛЬНЫХ ЦЕЛЕЙ И МЫ НИКОГО НЕ ХОТИМ ОСКОРБИТЬ ИЛИ УНИЗИТЬ",
         'software': "Софты",
-        'admins': "СПИСОК АДМИНОВ",
+        'admins': "Администрация",
         'partners': "Партнёры",
-        'main_admin': "Главный",
+        'main_admin': "Главный администратор",
         'creators': "Создатели",
-        'senior_admins': "Старшие админы",
-        'junior_admins': "Младшие админы",
+        'senior_admins': "Старшие администраторы",
+        'junior_admins': "Младшие администраторы",
         'senior_mods': "Старшие модераторы",
         'copyright': "© 2025 PrankVzlom. Все права защищены.",
         'accept': "Принять",
         'modal_title': "ВНИМАНИЕ",
-        'modal_content': "Этот сайт создан исключительно в развлекательных целях. Мы не хотим никого оскорбить или унизить. Продолжая, вы подтверждаете, что понимаете это."
+        'modal_content': "Этот сайт создан исключительно в развлекательных целях. Мы не хотим никого оскорбить или унизить.",
+        'links': {
+            'official_channel': "ОФИЦИАЛЬНЫЙ КАНАЛ",
+            'redirect': "ПЕРЕХОДНИК",
+            'chat': "ЧАТ",
+            'tutorial': "ТУТОРИАЛ ПО КАМЕРАМ",
+            'audio': "АУДИО КАНАЛ",
+            'bot': "БОТ",
+            'tiktok': "ТИК ТОК",
+            'support': "ПОДДЕРЖКА"
+        },
+        'partner_owl': "Совиный патруль"
     },
     'en': {
         'info_title': "PrankVzlom 📹📔",
-        'disclaimer': "THE SITE IS MADE FOR ENTERTAINMENT PURPOSES...",
+        'disclaimer': "THIS SITE IS FOR ENTERTAINMENT PURPOSES ONLY AND WE DON'T WANT TO OFFEND OR HUMILIATE ANYONE",
         'software': "Software",
-        'admins': "ADMINS LIST",
+        'admins': "Administration",
         'partners': "Partners",
         'main_admin': "Main Admin",
         'creators': "Creators",
@@ -49,26 +59,39 @@ translations = {
         'copyright': "© 2025 PrankVzlom. All rights reserved.",
         'accept': "Accept",
         'modal_title': "WARNING",
-        'modal_content': "This site is made for entertainment purposes only..."
+        'modal_content': "This site is made for entertainment purposes only. We don't want to offend or humiliate anyone.",
+        'links': {
+            'official_channel': "OFFICIAL CHANNEL",
+            'redirect': "REDIRECT",
+            'chat': "CHAT",
+            'tutorial': "CAMERA TUTORIAL",
+            'audio': "AUDIO CHANNEL",
+            'bot': "BOT",
+            'tiktok': "TIKTOK",
+            'support': "SUPPORT"
+        },
+        'partner_owl': "Owl Patrol"
     }
 }
 
 @app.route('/')
 def index():
     lang = session.get('lang', 'ru')
+    t = translations[lang]
+    
     site_data = {
         "info": {
-            "title": translations[lang]['info_title'],
-            "description": translations[lang]['disclaimer'],
+            "title": t['info_title'],
+            "description": t['disclaimer'],
             "links": [
-                {"name": "ОФИЦИАЛЬНЫЙ КАНАЛ", "url": "https://t.me/+_dzIExBP_yViZDk9"},
-                {"name": "ПЕРЕХОДНИК", "url": "https://t.me/prankzvon"},
-                {"name": "ЧАТ", "url": "https://t.me/+gUAplPwH9GhiMDg1"},
-                {"name": "ТУТОР ПО КАМЕРАМ", "url": "https://t.me/+1ZZKZBWjTHxjMDQ8"},
-                {"name": "КАНАЛ С АУДИО", "url": "https://t.me/+Y77I8AtvLNM0OGUy"},
-                {"name": "БОТ", "url": "https://t.me/prankvzlomnewbot"},
-                {"name": "ТИК ТОК", "url": "https://www.tiktok.com/@jiarbuz"},
-                {"name": "ПОДДЕРЖКА PRANKVZLOM", "url": "https://t.me/PrankVzlomUnban"}
+                {"name": t['links']['official_channel'], "url": "https://t.me/+_dzIExBP_yViZDk9"},
+                {"name": t['links']['redirect'], "url": "https://t.me/prankzvon"},
+                {"name": t['links']['chat'], "url": "https://t.me/+gUAplPwH9GhiMDg1"},
+                {"name": t['links']['tutorial'], "url": "https://t.me/+1ZZKZBWjTHxjMDQ8"},
+                {"name": t['links']['audio'], "url": "https://t.me/+Y77I8AtvLNM0OGUy"},
+                {"name": t['links']['bot'], "url": "https://t.me/prankvzlomnewbot"},
+                {"name": t['links']['tiktok'], "url": "https://www.tiktok.com/@jiarbuz"},
+                {"name": t['links']['support'], "url": "https://t.me/PrankVzlomUnban"}
             ]
         },
         "software": [
@@ -76,36 +99,106 @@ def index():
             {"name": "Nesca", "url": "https://cloud.mail.ru/public/J2sJ/3vuy7XC1n"},
             {"name": "Noon", "url": "https://cloud.mail.ru/public/4Cmj/yMeVGQXE6"},
             {"name": "Ingram", "url": "https://cloud.mail.ru/public/nPCQ/JA73sB4tq"},
-            {"name": "SoundPad", "url": "https://mega.nz/file/Ck4jhZBL#bXmvrKCquhJrt2hMlHiV2QfpzMm3uj_lLjv9yFLEjgA"}
+            {"name": "SoundPad", "url": "https://mega.nz/file/Ck4jhZBL#bXmvrKCquhJrt2hMlHiV2QfpzMm3uj_lLjv9yFLEjgA"},
+            {"name": "iVMS-4200", "url": "https://cloud.mail.ru/public/8t1M/g5zfvA8Lq"},
+            {"name": "MVFPS", "url": "https://cloud.mail.ru/public/26ae/58VrzdvYT"},
+            {"name": "KPortScan", "url": "https://cloud.mail.ru/public/yrup/9PQyDe86G"}
         ],
         "admins": {
-            translations[lang]['main_admin']: [{"name": "Православный Бес", "url": "https://t.me/bes689"}],
-            translations[lang]['creators']: [
-                {"name": "Everyday", "url": "https://t.me/mobile_everyday"},
-                {"name": "Андрей", "url": "https://t.me/prankzvon231"},
-                {"name": "Lucper", "url": "https://t.me/lucper1"}
+            t['main_admin']: [
+                {
+                    "name": "Православный Бес", 
+                    "url": "https://t.me/bes689",
+                    "avatar": "https://ltdfoto.ru/images/2025/07/25/photo_2025-07-25_06-19-13.jpg"
+                }
             ],
-            translations[lang]['senior_admins']: [
-                {"name": "Диванный воин Кчау", "url": "https://t.me/bestanov"},
-                {"name": "JIARBUZ.exe", "url": "https://t.me/jiarbuz"},
-                {"name": "ximi13p", "url": "https://t.me/ximi13p"},
-                {"name": "Цыфра", "url": "https://t.me/himera_unturned"}
+            t['creators']: [
+                {
+                    "name": "Everyday", 
+                    "url": "https://t.me/mobile_everyday",
+                    "avatar": "https://i.ibb.co/spKRJcmK/photo-2025-05-23-16-45-24.jpg"
+                },
+                {
+                    "name": "Андрей", 
+                    "url": "https://t.me/prankzvon231",
+                    "avatar": None
+                },
+                {
+                    "name": "Lucper", 
+                    "url": "https://t.me/lucper1",
+                    "avatar": "https://i.ibb.co/TMbSG0jp/photo-2025-07-20-01-44-45-2.gif"
+                }
             ],
-            translations[lang]['junior_admins']: [
-                {"name": "k3stovski", "url": "https://t.me/k3stovski"},
-                {"name": "Наполеонский пистолэтPRNKZV", "url": "https://t.me/prnkzvn"},
-                {"name": "жук", "url": "https://t.me/werwse"},
-                {"name": "kronaфacia", "url": "https://t.me/kronaphasia"}
+            t['senior_admins']: [
+                {
+                    "name": "Диванный воин Кчау", 
+                    "url": "https://t.me/bestanov",
+                    "avatar": "https://i.ibb.co/rKLcJ70c/photo-2025-04-23-02-37-37.jpg"
+                },
+                {
+                    "name": "JIARBUZ.exe", 
+                    "url": "https://t.me/jiarbuz",
+                    "avatar": "https://i.ibb.co/kgBVDqM8/photo-2025-06-10-15-16-39.jpg"
+                },
+                {
+                    "name": "ximi13p", 
+                    "url": "https://t.me/ximi13p",
+                    "avatar": None
+                }
             ],
-            translations[lang]['senior_mods']: [
-                {"name": "aiocryp", "url": "https://t.me/aiocryp"},
-                {"name": "саня шпалин", "url": "https://t.me/sanya_shpalka"}
+            t['junior_admins']: [
+                {
+                    "name": "k3stovski", 
+                    "url": "https://t.me/k3stovski",
+                    "avatar": "https://ltdfoto.ru/images/2025/07/25/photo_2025-07-24_23-01-05.jpg"
+                },
+                {
+                    "name": "kronaфacia", 
+                    "url": "https://t.me/kronaphasia",
+                    "avatar": None
+                },
+                {
+                    "name": "Жук", 
+                    "url": "https://t.me/werwse",
+                    "avatar": "https://ltdfoto.ru/images/2025/07/25/photo_2025-07-23_22-07-41.md.jpg"
+                },
+                {
+                    "name": "Цыфра", 
+                    "url": "https://t.me/himera_unturned",
+                    "avatar": "https://ltdfoto.ru/images/2025/07/25/photo_2025-07-01_00-52-02.md.jpg"
+                },
+                {
+                    "name": "Наполеонский пистолэт", 
+                    "url": "https://t.me/prnkzvn",
+                    "avatar": "https://ltdfoto.ru/images/2025/07/25/photo_2025-07-20_04-28-28.jpg"
+                }
+            ],
+            t['senior_mods']: [
+                {
+                    "name": "Paul Du Rove", 
+                    "url": "tg://openmessage?user_id=7401067755",
+                    "avatar": "https://ltdfoto.ru/images/2025/07/25/photo_2025-05-03_16-27-45.jpg"
+                },
+                {
+                    "name": "aiocryp", 
+                    "url": "https://t.me/aiocryp",
+                    "avatar": "https://ltdfoto.ru/images/2025/07/25/photo_2025-07-25_00-37-50.jpg"
+                },
+                {
+                    "name": "саня шпалин", 
+                    "url": "https://t.me/sanya_shpalka",
+                    "avatar": "https://ltdfoto.ru/images/2025/07/25/photo_2025-07-22_03-52-56.md.jpg"
+                }
             ]
         },
         "partners": [
-            {"name": "Совиный патруль", "url": "https://t.me/+x90aZWxcI8ljZmQy"}
+            {
+                "name": t['partner_owl'],
+                "url": "https://t.me/+x90aZWxcI8ljZmQy",
+                "avatar": "https://ltdfoto.ru/images/2025/07/25/photo_2025-06-30_23-03-01.jpg"
+            }
         ],
-        "translations": translations[lang]
+        "translations": t
     }
     return render_template('index.html', data=site_data, current_lang=lang)
 
@@ -113,8 +206,7 @@ def index():
 def set_language(lang):
     if lang in ['ru', 'en']:
         session['lang'] = lang
-    return '', 204  # No content
+    return '', 204
 
 if __name__ == '__main__':
     app.run(debug=True)
-
